a6a81bbe0c0259b04bf19563c936f49c81b72f9f
bkim222 <benjamin.j.kim@email.ucr.edu>
2025-02-18 03:21:20 -0800

    Create flask_camera.py

---
+++ draft_ws/flask_camera.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response
+import cv2
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(1)  # eventually change 1 to our luxonis url. 1 is for macbook cam
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)

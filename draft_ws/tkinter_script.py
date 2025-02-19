debab1b4cd42cb9ed15d10399165590fd30098a9
PryceMatsu <prycematsu@gmail.com>
2025-02-18 20:12:04 -0800

    Delete draft_ws/tkinter_script.py

--- draft_ws/tkinter_script.py
+++
@@ -1,7 +0,0 @@
-import time
-count = 0
-while True:
-    count = count+1
-    ct = count
-    print(f"[TRANSMISSION RECEIVED] Hello World{ct}")
-    time.sleep(2)  # Wait 2 seconds between transmissions
